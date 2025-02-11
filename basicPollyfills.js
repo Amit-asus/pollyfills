5ad169490207244ae58f91cb5786fa8b5c39fc2f
CoderSpace <coderspace@Amits-MacBook-Air.local>
2025-02-12 00:04:46 +0530

    added includes polyfill method

--- basicPollyfills.js
+++ basicPollyfills.js
@@ -16,8 +16,26 @@ if (!Array.prototype.myMap) {
   };
 }
 
-const arr = [1, 2, 3, 4, 5];
+//test 
+// const arr = [1, 2, 3, 4, 5];
+// const result = arr.myMap((a) => a * 2);
+// console.log(result);
 
-const result = arr.myMap((a) => a * 2);
+//includes   method in js
+if (!Array.prototype.myInclude) {
+  Array.prototype.myInclude = function (element) {
+    if (!element) {
+      throw new TypeError("no element in provided ");
+    }
 
-console.log(result);
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === element) return true;
+    }
+    return false;
+  };
+}
+
+//test 
+// const brr = [1, 2, 3, 4];
+// console.log(brr.myInclude(3));
+// console.log(brr.myInclude(5));
